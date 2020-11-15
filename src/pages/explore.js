6ab15abba3e19c380f3ec4aea4dc68d07ce5b0d3
Nabu <alakkat.n@gmail.com>
2020-11-15 18:55:32 -0500

    responsive test

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -44,16 +44,18 @@ const ExplorePage = () => {
       <Layout theme={"light"}>
         <Head title="Explore"/>
         <Link style={{textDecoration:"none"}}to={`/blog/${data.contentfulFeaturedPost.slug}`}>
-          <div style={{
-              background: `-webkit-gradient(linear,left top, left bottom,from(rgba(0, 0, 0, 0.5)),to(rgba(0, 0, 0, 0.3))),url(${data.contentfulFeaturedPost.thumbnailImage.file.url})`,
-              backgroundSize:"cover",
-              backgroundPosition:"center",
-              height:"100vh",
-              backgroundAttachment:"fixed"
-          }} className={exploreStyles.hero}>
-            <div className={exploreStyles.feature}>
-              <h2 className={exploreStyles.featureTitle}>{data.contentfulFeaturedPost.title}</h2>
-              <p>{data.contentfulFeaturedPost.excerpt.excerpt} </p>
+          <div className={exploreStyles.container}>
+            <div style={{
+                background: `-webkit-gradient(linear,left top, left bottom,from(rgba(0, 0, 0, 0.5)),to(rgba(0, 0, 0, 0.3))),url(${data.contentfulFeaturedPost.thumbnailImage.file.url})`,
+                backgroundSize:"cover",
+                backgroundPosition:"center",
+                height:"100vh",
+                backgroundAttachment:"fixed"
+            }} className={exploreStyles.hero}>
+              <div className={exploreStyles.feature}>
+                <h2 className={exploreStyles.featureTitle}>{data.contentfulFeaturedPost.title}</h2>
+                <p>{data.contentfulFeaturedPost.excerpt.excerpt} </p>
+              </div>
             </div>
           </div>
           </Link>
