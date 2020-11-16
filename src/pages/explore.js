f9a6968a86479cc8d0624d718619dfbdc5ea66b4
Nabu <alakkat.n@gmail.com>
2020-11-16 17:09:57 -0500

    feature thumbnail test

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -7,13 +7,6 @@ import FilterBar from '../components/FilterBar'
 import PostsList from '../components/PostsList'
 import Head from '../components/Head'
 
-let vh = window.innerHeight*0.01
-document.documentElement.style.setProperty('--vh', `${vh}px`)
-
-window.addEventListener('resize', () => {
-  vh = window.innerHeight*0.01
-  document.documentElement.style.setProperty('--vh', `${vh}px`)
-})
 
 
 const ExplorePage = () => {
