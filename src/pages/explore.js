f37ee75e203c353b9a39644f51148c59dcb0c2cd
Nabu <alakkat.n@gmail.com>
2020-11-16 16:32:34 -0500

    responsive test 11

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -7,7 +7,13 @@ import FilterBar from '../components/FilterBar'
 import PostsList from '../components/PostsList'
 import Head from '../components/Head'
 
+let vh = window.innerHeight*0.01
+document.documentElement.style.setProperty('--vh', `${vh}px`)
 
+window.addEventListener('resize', () => {
+  vh = window.innerHeight*0.01
+  document.documentElement.style.setProperty('--vh', `${vh}px`)
+})
 
 
 const ExplorePage = () => {
